8b1cf949aac8c02801b8a9af742d387cf7cc4d8b
Ivanna Ostrovets <ivanna.ostrovets@gmail.com>
2023-09-03 02:30:14 +0300

    Run app on 3000 port always

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,10 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    port: 3000,
+    strictPort: true,
+  },
   plugins: [
     vue(),
   ],
