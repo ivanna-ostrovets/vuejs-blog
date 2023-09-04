425ea3b9a5e0cf854ab1096af4993ee6cf51f6e6
Ivanna Ostrovets <ivanna.ostrovets@gmail.com>
2023-09-05 02:45:39 +0300

    Fix Tailwind colors warning

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,12 +6,13 @@ const colors = require('tailwindcss/colors');
 module.exports = {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    colors: {
-      ...colors,
-      primary: '#1C1C1C',
-      text: colors.black,
-      textSecondary: 'rgba(28, 28, 28, 0.50)',
-      inputBackground: '#F8F8F8',
+    extend: {
+      colors: {
+        primary: '#1C1C1C',
+        text: colors.black,
+        textSecondary: 'rgba(28, 28, 28, 0.50)',
+        inputBackground: '#F8F8F8',
+      },
     },
     fontFamily: {
       text: ['Open Sans', 'sans-serif'],
